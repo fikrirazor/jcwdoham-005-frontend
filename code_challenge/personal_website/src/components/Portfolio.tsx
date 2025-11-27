614e20e42801f28072a862053c21cf2d3cb13b6d
fikrirazor <fikrirazor@outlook.co.id>
2025-11-28 05:36:48 +0700

    feat: About +Portfolio + skills +contact

---
+++ code_challenge/personal_website/src/components/Portfolio.tsx
@@ -0,0 +1,10 @@
+function Portfolio() {
+  return (
+    <section className="Portfolio  text-center h-screen flex flex-col justify-center items-center text-white bg-black">
+      <h1>Portfolio</h1>
+      <div className="projects "></div>
+    </section>
+  );
+}
+
+export default Portfolio;
--- code_challenge/personal_website/src/pages/Home.tsx
+++ code_challenge/personal_website/src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
 
 function Home() {
   return (
-    <main className="   ">
+    <main className="  font-jakarta ">
       <nav className="lg:fixed lg:right-8 lg:top-0 lg:h-full lg:w-64 sticky top-0 p-1 relative nav-dashed-border shadow-[4px_0_6px_rgba(251,191,36,0.3)] animate-float z-50">  
-        <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-end lg:flex-col lg:items-end p-4 pr-8 text-white">
+        <div className="max-w-7xl mx-auto flex flex-wrap items-center justify-end lg:flex-col lg:items-end p-4 pr-8 text-white">
           <ul className="flex flex-col lg:flex-col space-y-4 lg:space-y-6 w-full lg:w-auto">
             <li className="lg:text-right">
               <a href="#" className="hover:text-gray-300  block">
@@ -35,28 +39,9 @@ function Home() {
         </div>
       </nav>
       <Hero />
-      <section className="About text-center h-screen flex flex-col justify-center items-center text-white bg-black">
-        <p>
-          {" "}
-          I am a web developer with a strong interest in building clean,
-          efficient, and user-focused digital experiences. I started learning
-          web development out of curiosity, and over time it grew into a passion
-          that shaped my career. I have experience working on small to mid-scale
-          projects, including landing pages, dashboards, and API-driven
-          applications.
-        </p>
-        <div className="skills  text-center h-screen flex flex-col justify-center items-center text-white bg-black">
-          <div>python</div>
-          <div>javascript</div>
-          <div>Typescript</div>
-          <div>Git</div>
-          <div>React</div>
-        </div>
-      </section>
-      <section className="Portfolio  text-center h-screen flex flex-col justify-center items-center text-white bg-black">
-        <h1>Portfolio</h1>
-        <div className="projects "></div>
-      </section>
+      <About />
+      <Skills  />
+      <Portfolio />
       <section className="Experience  text-center h-screen flex flex-col justify-center items-center text-white bg-black">
         <h1>Experience</h1>
       </section>
@@ -67,6 +52,7 @@ function Home() {
           always available to help others."
         </p>
       </section>
+      <Contact />
     </main>
   );
 }
